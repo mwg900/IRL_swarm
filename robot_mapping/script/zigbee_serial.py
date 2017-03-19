6f638b0b15637d82dd1974d23fedf06f559b85d4
Unknown <mwg900@naver.com>
2017-03-19 22:04:42 +0900

    0.3.3
    시리얼 부분 수정

--- robot_mapping/script/zigbee_serial.py
+++ robot_mapping/script/zigbee_serial.py
@@ -10,7 +10,7 @@ class zigbee:
     def __init__(self):
         rospy.init_node('zigbee_serial')
         position_sub = rospy.Subscriber("/position", Slavepos, self.callback, queue_size= 100)
-        #self.pub = rospy.Publisher('slave_sig', Serialmsg, queue_size=10)
+        self.pub = rospy.Publisher('/Serial', Serialmsg, queue_size=10)
         self.ser = serial.Serial('/dev/Zigbee', 115200) #시리얼 통신용 변수. 115200의 baudrate를 가짐
         
         
@@ -20,8 +20,8 @@ class zigbee:
     def callback(self, msg):
         
         s1_dist_mm = "%.0f" %(msg.s1_dist*1000)
-        #self.send_msg = "<"+s1_dist_mm+","+str(msg.s1_ang)+","+"0,0,0,0,"+str(msg.m_ang)+">"
         self.send_msg = "<"+str(msg.m_ang)+","+s1_dist_mm+","+str(msg.s1_ang)+","+"0,0,0,0"+">"        
+        self.ser.writelines(self.send_msg)
         #rospy.loginfo("<m_w,s1_d,s1_w,s2_d,s2_w,s3_d,s3_w> = %s" ,self.send_msg)
     
     def talker(self):
@@ -29,31 +29,28 @@ class zigbee:
         #msg.flag.data = True
         
         while not rospy.is_shutdown():
-            '''
-            receive = ser.read()
-            rospy.loginfo("id: %s received", receive)
             
+            #receive = ser.read()
+            receive = 1
+            rospy.loginfo("id: %s received", receive)
+            msg = Serialmsg()
             if receive is not '':
                 msg.time.data = rospy.Time.now()
-                #msg.id = int(receive)
-                
-                #self.pub.publish(msg)           
-            
+                msg.id = int(receive)
+                self.pub.publish(msg)           
                 #Zigbee serial Message 전송
                 #send_msg = "return <%d>" %msg.id
                 
                 #print(send_msg)
-            '''
-            self.ser.writelines(self.send_msg)
+
+            #self.ser.writelines(self.send_msg)
             print(self.send_msg)
             #self.r.sleep() 최대한 빠른 속도로 동작하기 위하여 주석처리
-            self.r.sleep()
         self.ser.close()
 if __name__ == '__main__':
     try:
         main = zigbee()
         main.talker()
-        #rospy.spin()
     except rospy.ROSInterruptException: pass
 
     
